0bd089a4cc838b6afb69ea58873dd19facf33374
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-12-16 13:06:16 -0600

    created more test for print fuction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,29 +3,36 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        char value = 'A';
-        switch (value) {
-            case 'A':
-                System.out.println("Value was A");
-                break;
-            case 'B':
-                System.out.println("Value was A");
-                break;
-            case 'C':
-                System.out.println("Value was A");
-                break;
-            case 'D':
-                System.out.println("Value was A");
-                break;
-            case 'E':
-                System.out.println("Value was A");
-                break;
-            default:
-                System.out.println("Not found");
-                break;
-        }
-        int day = 1;
-        printDayOfTheWeek(day);
+//        char value = 'A';
+//        switch (value) {
+//            case 'A':
+//                System.out.println("Value was A");
+//                break;
+//            case 'B':
+//                System.out.println("Value was A");
+//                break;
+//            case 'C':
+//                System.out.println("Value was A");
+//                break;
+//            case 'D':
+//                System.out.println("Value was A");
+//                break;
+//            case 'E':
+//                System.out.println("Value was A");
+//                break;
+//            default:
+//                System.out.println("Not found");
+//                break;
+//        }
+        printDayOfTheWeek(-1);
+        printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(6);
+        printDayOfTheWeek(7);
+        printDayOfTheWeek(40303);
 
     }
 
