d198000f3a9576fe53d07bc2d87455910a14925e
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-12-16 12:04:01 -0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char value = 'A';
+        switch (value) {
+            case 'A':
+                System.out.println("Value was A");
+                break;
+            case 'B':
+                System.out.println("Value was A");
+                break;
+            case 'C':
+                System.out.println("Value was A");
+                break;
+            case 'D':
+                System.out.println("Value was A");
+                break;
+            case 'E':
+                System.out.println("Value was A");
+                break;
+            default:
+                System.out.println("Not found");
+                break;
+        }
+    }
+}
